326cf19f208214db8b5040c3d9ed9802f0b51ff1
ThetaRichPC <10991478@uvu.edu>
2024-02-23 15:12:06 -0700

    Finished createcontrols script for lab7

--- PythonScripts/CreateControlsForJoints.py
+++ PythonScripts/CreateControlsForJoints.py
@@ -1,11 +1,14 @@
+#Used the help of Chat in making parts of this script
+
+
 import maya.cmds as cmds
 
-def create_control_and_group(joint):
+def create_control_and_group(joint, colorRL):
     # Get joint position
     joint_position = cmds.xform(joint, q=True, ws=True, t=True)
     
     # Get joint orientation
-    joint_orientation = cmds.joint(joint, q=True, orientation = True)
+    joint_orientation = cmds.xform(joint, q=True, ws=True, ro=True)
     
     # Create control curve (circle)
     control_curve = cmds.circle(normal=(0, 1, 0), radius=1)[0]
@@ -24,7 +27,7 @@ def create_control_and_group(joint):
     
     # Parent control under parent group
     cmds.parent(control_curve, parent_group)
-    cmds.setAttr(f"{control_curve}.rx", 90)
+    cmds.setAttr(f"{control_curve}.rz", 90)
     cmds.makeIdentity(control_curve, apply = True, rotate = True)
     
     # Rename control and parent group
@@ -32,6 +35,14 @@ def create_control_and_group(joint):
     parent_group_name = joint + '_grp'
     cmds.rename(control_curve, control_name)
     cmds.rename(parent_group, parent_group_name)
+
+    # Changing colors for left and right side controls
+    if control_name[:2] == "R_" and colorRL == True:
+        cmds.setAttr((control_name + ".overrideEnabled"), 1)
+        cmds.setAttr((control_name + ".overrideColor"), 13)
+    elif control_name[:2] == "L_" and colorRL == True:
+        cmds.setAttr((control_name + ".overrideEnabled"), 1)
+        cmds.setAttr((control_name + ".overrideColor"), 6)
     
     return control_name, parent_group_name
 
@@ -44,7 +55,7 @@ def main():
         return
     
     for joint in selected_joints:
-        control_name, parent_group_name = create_control_and_group(joint)
+        control_name, parent_group_name = create_control_and_group(joint, True)
         print(f"Created control: {control_name}, parent group: {parent_group_name}")
 
 if __name__ == "__main__":
